c35ae9f2bfba6daeea869b30480725fc3d257a16
rohit502dhurve-cloud <rohit502dhurve@gmail.com>
2026-04-06 13:45:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -571,8 +571,8 @@ init_db_safe()
 if os.path.exists("villages.csv"):
     bulk_insert_villages()   # 🔥 IMPORTANT (1 बार चलाना है)
 
-# if os.path.exists("personnel.csv"):
-#   bulk_insert_personnel()
+if os.path.exists("personnel.csv"):
+    bulk_insert_personnel()
 
 if __name__ == '__main__':    
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
