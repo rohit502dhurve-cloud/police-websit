8f738511241ee6b299c5a77b11e6e48cbf22c8a3
rohit502dhurve-cloud <rohit502dhurve@gmail.com>
2026-05-04 16:10:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -877,6 +877,18 @@ def delete_personnel(id):
 
     return redirect('/personnel')
 
+@app.route('/delete_posting/<int:id>')
+def delete_posting(id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    cur.execute("DELETE FROM posting_history WHERE id = %s", (id,))
+    conn.commit()
+
+    conn.close()
+
+    return redirect(url_for('personnel_history'))
+
 @app.route('/add_personnel_page')
 def add_personnel_page():
     if not session.get("personnel_admin"):
